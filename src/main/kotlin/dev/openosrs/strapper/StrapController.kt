fcf905f9848074290e1624949047cc489f16c800
PKLite <stonewall@pklite.xyz>
2019-09-21 09:53:35 -0400

    Add command line args to use github secrets
    Signed-off-by: PKLite <stonewall@pklite.xyz>

--- src/main/kotlin/dev/openosrs/strapper/StrapController.kt
+++ src/main/kotlin/dev/openosrs/strapper/StrapController.kt
@@ -1,6 +1,8 @@
 package dev.openosrs.strapper
 
 
+import com.google.common.hash.Hashing
+import com.google.common.io.Files
 import com.google.gson.Gson
 import dev.openosrs.strapper.uploaders.FTPUploader
 import mu.KotlinLogging
@@ -9,8 +11,6 @@ import org.eclipse.jgit.errors.RepositoryNotFoundException
 import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder
 import java.io.File
-import java.nio.charset.Charset
-import java.nio.file.Files
 import java.util.*
 import javax.swing.JOptionPane
 import kotlin.collections.HashMap
@@ -21,6 +21,8 @@ private val log = KotlinLogging.logger("StrapLogger")
 class StrapController {
     companion object {
         var mode = "test"
+        var user = ""
+        var p = ""
     }
 
     private val bootstrapLoader = BootstrapLoader()
@@ -36,7 +38,7 @@ class StrapController {
         try {
             val fr = FileRepositoryBuilder().setMustExist(true).setGitDir(File(dir, "\\.git"))
                 .setMustExist(true).build()
-            val head = with(RevWalk(fr))
+            val head = with (RevWalk(fr))
             {
                 this.parseCommit(fr.getRef("refs/heads/master").leaf.objectId).name
             }
@@ -46,24 +48,23 @@ class StrapController {
             val projectVersion = properties.getProperty("runelite.plus.version")
             val oldArtifacts = bootstrap.artifacts.filter { !it.name.contains("SNAPSHOT") }
             bootstrap.artifacts = oldArtifacts
-            val uploader = FTPUploader(
-                JOptionPane.showInputDialog("Enter FTP login username"),
-                JOptionPane.showInputDialog("Enter FTP password")
-            )
+            val uploader = when (mode) {
+                "nighlty" -> FTPUploader(user, p)
+                else -> FTPUploader(JOptionPane.showInputDialog("Enter FTP login username"), JOptionPane.showInputDialog("Enter FTP password"))
+            }
             val artifactFiles = HashMap<String, File>()
+
             if (uploader.connect()) {
                 for (s in artifactsList) {
                     if (s.contains("runelite-client")) {
-                        val fName =
-                            "${s.replace("runelite-", "").split("/")[0]}-$rlVersion.${bootstrap.client.extension}"
+                        val fName = "${s.replace("runelite-", "").split("/")[0]}-$rlVersion.${bootstrap.client.extension}"
                         artifactFiles[fName] = File(File(dir, s), fName)
                         val newName = uploader.upload(artifactFiles[fName]!!)
                         val file = artifactFiles[fName]!!
                         val size = file.length().toString()
                         val location = "http://download.tuxfamily.org/rlplus/$mode/$newName"
                         val hash = DigestUtils.sha512Hex(file.readBytes())
-                        bootstrap.artifacts =
-                            bootstrap.artifacts.plus(Bootstrap.Artifact(hash, newName, location, size))
+                        bootstrap.artifacts = bootstrap.artifacts.plus(Bootstrap.Artifact(hash, newName, location, size))
                     } else {
                         val fName = "${s.split("/")[0]}-$rlVersion.${bootstrap.client.extension}"
                         artifactFiles[fName] = File(File(dir, s), fName)
@@ -71,32 +72,21 @@ class StrapController {
                         val file = artifactFiles[fName]!!
                         val size = file.length().toString()
                         val location = "http://download.tuxfamily.org/rlplus/$mode/$newName"
-                        val hash = DigestUtils.sha256Hex(file.readBytes())
-                        bootstrap.artifacts =
-                            bootstrap.artifacts.plus(Bootstrap.Artifact(hash, newName, location, size))
+                        val hash =  with (Hashing.sha256())
+                        {
+                            Files.asByteSource(file).hash(this).toString()
+                        }
+                        String(DigestUtils.sha256(file.readBytes()))
+                        bootstrap.artifacts = bootstrap.artifacts.plus(Bootstrap.Artifact(hash, newName, location, size))
                     }
                 }
             }
             bootstrap.projectVersion = projectVersion
             bootstrap.buildCommit = head.toString()
-            bootstrap.client = Bootstrap.Client(
-                "client", "", "jar",
-                "net.runelite", "", rlVersion
-            )
-            Files.writeString(
-                File(
-                    "bootstrap${when (mode) {
-                        "staging" -> "-staging"
-                        else -> ""
-                    }}.json"
-                ).toPath(),
-                Gson().newBuilder().setPrettyPrinting().disableHtmlEscaping().create().toJson(bootstrap),
-                Charset.defaultCharset()
-            )
-            JOptionPane.showMessageDialog(
-                null,
-                "Bootstrapping is complete. Don't forget to PR the bootstrap file to the maven-repo"
-            )
+            bootstrap.client = Bootstrap.Client("client", "", "jar",
+                "net.runelite", "", rlVersion)
+            java.nio.file.Files.writeString(File("bootstrap.json").toPath(), Gson().newBuilder().setPrettyPrinting().create().toJson(bootstrap))
+            JOptionPane.showMessageDialog(null, "Bootstrapping is complete. Don't forget to PR the bootstrap file to the maven-repo")
         } catch (e: RepositoryNotFoundException) {
             e.printStackTrace()
         }
--- src/main/kotlin/dev/openosrs/strapper/Strapper.kt
+++ src/main/kotlin/dev/openosrs/strapper/Strapper.kt
@@ -1,36 +1,50 @@
 package dev.openosrs.strapper
 
 import dev.openosrs.strapper.exceptions.VersionException
+import java.io.File
 import javax.swing.JFileChooser
 import javax.swing.JOptionPane
 import kotlin.system.exitProcess
 
-fun main() {
+fun main(args: Array<String>) {
+    if (args.isNotEmpty())
+    {
+        StrapController.mode = "nightly"
+        StrapController.user = args[0]
+        StrapController.p = args[1]
+    }
     BootstrapLoader().loadBootStrap()
     val controller = StrapController()
-    StrapController.mode = when (JOptionPane.showOptionDialog(
-        null,
-        "Select Bootstrap mode",
-        "OpenOSRS Bootstrapper",
-        JOptionPane.DEFAULT_OPTION,
-        JOptionPane.INFORMATION_MESSAGE,
-        null,
-        arrayOf("TEST", "STAGING", "LIVE"),
-        null
-    )) {
-        0 -> "test"
-        1 -> "staging"
-        2 -> "live"
-        else -> exitProcess(-1)
+    when (StrapController.mode) {
+        "nightly" -> controller.strapArtifacts(File("./"))
+        else -> {
+            StrapController.mode = when (JOptionPane.showOptionDialog(
+                    null,
+                    "Select Bootstrap mode",
+                    "OpenOSRS Bootstrapper",
+                    JOptionPane.DEFAULT_OPTION,
+                    JOptionPane.INFORMATION_MESSAGE,
+                    null,
+                    arrayOf("TEST", "STAGING", "LIVE"),
+                    null
+            )) {
+                0 -> "test"
+                1 -> "staging"
+                2 -> "live"
+                else -> exitProcess(-1)
+            }
+
+            val f = JFileChooser()
+            f.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
+            f.dialogTitle = "Select the root directory of OpenOSRS"
+            if (f.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+                controller.strapArtifacts(f.selectedFile)
+            }
+            else
+                throw VersionException(Exception("Couldn't find git repo at selected directory"))
+
+        }
     }
 
-    val f = JFileChooser()
-    f.fileSelectionMode = JFileChooser.DIRECTORIES_ONLY
-    f.dialogTitle = "Select the root directory of OpenOSRS"
-    if (f.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-        controller.strapArtifacts(f.selectedFile)
-    }
-        else
-            throw VersionException(Exception("Couldn't find git repo at selected directory"))
 
 }
