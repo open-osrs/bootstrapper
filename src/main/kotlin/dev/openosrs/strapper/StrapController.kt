b4438c223e9e59b0de44745a604e9f5ce1a7f7f0
PKLite <stonewall@pklite.xyz>
2019-09-21 13:18:28 -0400

    more strap preparation
    Signed-off-by: PKLite <stonewall@pklite.xyz>

--- src/main/kotlin/dev/openosrs/strapper/StrapController.kt
+++ src/main/kotlin/dev/openosrs/strapper/StrapController.kt
@@ -1,8 +1,6 @@
 package dev.openosrs.strapper
 
 
-import com.google.common.hash.Hashing
-import com.google.common.io.Files
 import com.google.gson.Gson
 import dev.openosrs.strapper.uploaders.FTPUploader
 import mu.KotlinLogging
@@ -16,7 +14,7 @@ import javax.swing.JOptionPane
 import kotlin.collections.HashMap
 
 
-private val log = KotlinLogging.logger("StrapLogger")
+private val logger = KotlinLogging.logger("StrapLogger")
 
 class StrapController {
     companion object {
@@ -28,36 +26,43 @@ class StrapController {
     private val bootstrapLoader = BootstrapLoader()
     private var bootstrap = bootstrapLoader.loadBootStrap()
     private val artifactsList = listOf(
-        "runescape-api/build/libs", "runelite-client/build/libs", "injected-client/build/libs", "http-api/build/libs",
-        "runelite-api/build/libs", "runescape-api/build/libs"
+            "runescape-api/build/libs", "runelite-client/build/libs", "injected-client/build/libs", "http-api/build/libs",
+            "runelite-api/build/libs", "runescape-api/build/libs"
     )
 
 
     fun strapArtifacts(dir: File) {
+        logger.info { "attempting to strap artifacts from directory ${dir.name}" }
         val f = File(dir, "runelite-client/build/resources/main/runelite.plus.properties")
+
         try {
             val fr = FileRepositoryBuilder().setMustExist(true).setGitDir(File(dir, "\\.git"))
-                .setMustExist(true).build()
-            val head = with (RevWalk(fr))
+                    .setMustExist(true).build()
+            val head = with(RevWalk(fr))
             {
                 this.parseCommit(fr.getRef("refs/heads/master").leaf.objectId).name
             }
+            logger.info { "proceeding with git commit $head" }
             val properties = Properties()
             properties.load(f.inputStream())
+            logger.info { "attempting to load properties resource. . ." }
             val rlVersion = properties.getProperty("runelite.version")
             val projectVersion = properties.getProperty("runelite.plus.version")
+            logger.info { "proceeding with runelite version $rlVersion and openOSRS version $projectVersion" }
             val oldArtifacts = bootstrap.artifacts.filter { !it.name.contains("SNAPSHOT") }
             bootstrap.artifacts = oldArtifacts
             val uploader = when (mode) {
                 "nighlty" -> FTPUploader(user, p)
-                else -> FTPUploader(JOptionPane.showInputDialog("Enter FTP login username"), JOptionPane.showInputDialog("Enter FTP password"))
+                else -> FTPUploader(JOptionPane.showInputDialog("Enter FTP login username"),
+                        JOptionPane.showInputDialog("Enter FTP password"))
             }
             val artifactFiles = HashMap<String, File>()
-
             if (uploader.connect()) {
+                logger.info { "connected to FTP. . . now locating build artifacts. . ." }
                 for (s in artifactsList) {
                     if (s.contains("runelite-client")) {
-                        val fName = "${s.replace("runelite-", "").split("/")[0]}-$rlVersion.${bootstrap.client.extension}"
+                        val fName = "${s.replace("runelite-", "")
+                                .split("/")[0]}-$rlVersion.${bootstrap.client.extension}"
                         artifactFiles[fName] = File(File(dir, s), fName)
                         val newName = uploader.upload(artifactFiles[fName]!!)
                         val file = artifactFiles[fName]!!
@@ -72,31 +77,39 @@ class StrapController {
                         val file = artifactFiles[fName]!!
                         val size = file.length().toString()
                         val location = "http://download.tuxfamily.org/rlplus/$mode/$newName"
-                        val hash =  with (Hashing.sha256())
-                        {
-                            Files.asByteSource(file).hash(this).toString()
-                        }
-                        String(DigestUtils.sha256(file.readBytes()))
+                        val hash = DigestUtils.sha256Hex(file.readBytes())
                         bootstrap.artifacts = bootstrap.artifacts.plus(Bootstrap.Artifact(hash, newName, location, size))
                     }
+                    logger.info { "found artifact $s" }
+                }
+                logger.info { "building bootstrapper file. . ." }
+                bootstrap.projectVersion = projectVersion
+                bootstrap.buildCommit = head.toString()
+                bootstrap.client = Bootstrap.Client("client", "", "jar",
+                        "net.runelite", "", rlVersion)
+                val file = java.nio.file.Files.writeString(File("bootstrap-nightly.json").toPath(),
+                        Gson()
+                                .newBuilder()
+                                .setPrettyPrinting()
+                                .create()
+                                .toJson(bootstrap))
+                if (mode == "nightly") {
+                    uploader.uploadStrap(file.toFile())
+                }
+                if (mode != "nightly") {
+                    JOptionPane.showMessageDialog(null,
+                            "Bootstrapping is complete. Don't forget to PR the bootstrap file to the maven-repo")
                 }
             }
-            bootstrap.projectVersion = projectVersion
-            bootstrap.buildCommit = head.toString()
-            bootstrap.client = Bootstrap.Client("client", "", "jar",
-                "net.runelite", "", rlVersion)
-            java.nio.file.Files.writeString(File("bootstrap.json").toPath(), Gson().newBuilder().setPrettyPrinting().create().toJson(bootstrap))
-            JOptionPane.showMessageDialog(null, "Bootstrapping is complete. Don't forget to PR the bootstrap file to the maven-repo")
         } catch (e: RepositoryNotFoundException) {
             e.printStackTrace()
         }
-
     }
 
     fun validProjectDir(dir: File): Boolean {
         return try {
             val fr = FileRepositoryBuilder().setMustExist(true).setGitDir(File(dir, "\\.git"))
-                .setMustExist(true).build()
+                    .setMustExist(true).build()
             val head = fr.getRef("refs/heads/master")
             true
         } catch (e: RepositoryNotFoundException) {
--- src/main/kotlin/dev/openosrs/strapper/uploaders/FTPUploader.kt
+++ src/main/kotlin/dev/openosrs/strapper/uploaders/FTPUploader.kt
@@ -1,10 +1,12 @@
 package dev.openosrs.strapper.uploaders
 
+import com.google.common.io.Files
 import dev.openosrs.strapper.StrapController
 import mu.KotlinLogging
 import org.apache.commons.net.ftp.FTPClient
 import java.io.File
 import java.time.Instant
+import kotlin.system.exitProcess
 
 private val log = KotlinLogging.logger("FTPUploader")
 
@@ -22,13 +24,45 @@ class FTPUploader(private val user: String, private val password: String) {
 
      fun upload(file: File): String {
          ftp.reinitialize()
-
          log.info("Uploading ${file.absoluteFile}. . .")
          val inputStream = file.inputStream()
          val externalFileName = "${file.nameWithoutExtension}-$timeStamp.${file.extension}"
          var r = ftp.storeFile(externalFileName, inputStream)
          inputStream.close()
          log.info("Uploaded $externalFileName")
-        return externalFileName
+         return externalFileName
+    }
+
+    fun uploadStrap(file: File): Boolean {
+        ftp.reinitialize()
+        ftp.changeWorkingDirectory("rlplus/rlplus-repository")
+        when (ftp.retrieveFile("bootstrap-nightly.json",
+            File("bootstrap-nightly.json.bak").outputStream())) {
+            false -> {
+                log.error { "couldn't backup remote nightly json. it may not exist" }
+                exitProcess(420)
+            }
+            true -> {
+                log.info { "backed up nightly strap" }
+                return when (ftp.deleteFile("bootstrap-nightly.json")) {
+                    true -> {
+                        log.info { "deleted remote nightly" }
+                        deliverStrap(file)
+                    }
+                    false -> {
+                        log.info { "couldn't delete old strap." }
+                        deliverStrap(file)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun deliverStrap(file: File): Boolean {
+        log.info("Uploading ${file.absoluteFile}. . .")
+        var r = ftp.storeFile(file.name, file.inputStream())
+        file.inputStream().close()
+        log.info("Uploaded new strap")
+        return r
     }
 }
